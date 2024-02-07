6bfe7d9f140908f01a709ce55744bc7f919bed3c
Josue Zavala <josue.zavala123@gmail.com>
2024-02-06 20:54:32 -0600

    feat: Add QueryClientProvider

--- components/videos/videosComponent.tsx
+++ components/videos/videosComponent.tsx
@@ -5,19 +5,19 @@ import { httpClient } from "@/utils/http";
 import { useEffect } from "react";
 
 export const VideosComponent = () => {
-  /* const { data } = useVideos(); */
+  const { data } = useVideos();
 
-  const getVideosFromApi = async (): Promise<number> => {
-    /* const res = await fetch("http://localhost:8000/api/v1/videos"); */
+  /* const getVideosFromApi = async (): Promise<number> => {
+    //const res = await fetch("http://localhost:8000/api/v1/videos");
     const res = await httpClient.get(`/api/v1/videos`);
 
     console.log(res);
     return 2;
-  };
+  }; */
 
-  useEffect(() => {
+  /* useEffect(() => {
     getVideosFromApi();
-  }, []);
+  }, []); */
 
   return <div>Test</div>;
 };
---
+++ utils/react-query-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
+function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
+
+export default ReactQueryProvider;
